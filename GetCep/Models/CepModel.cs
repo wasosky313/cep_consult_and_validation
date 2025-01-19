d0c546a3f9f2b0e1006379cfc56b2f2040d56305
Andy Jimenez Reyes <andy.reyes@solfacil.com.br>
2025-01-19 00:31:00 -0300

    fix: CSharp variable name use JasonPropertyName to cast Upper and Lowercase in request and response

--- GetCep/Models/CepModel.cs
+++ GetCep/Models/CepModel.cs
@@ -1,18 +1,45 @@
+using System.Text.Json.Serialization;
+
 namespace GetCep.Models;
 
 public class CepResponse
 {
-    public required string cep { get; set; }
-    public required string logradouro { get; set; }
-    public required string bairro { get; set; }
-    public required string localidade { get; set; }
-    public required string uf { get; set; }
-    public required string estado { get; set; }
-    public string? complemento { get; set; }
-    public string? unidade { get; set; }
-    public string? regiao { get; set; }
-    public string? ibge { get; set; }
-    public string? gia { get; set; }
-    public string? ddd { get; set; }
-    public string? siafi { get; set; }
+    [JsonPropertyName("cep")]
+    public required string Cep { get; set; }
+    
+    [JsonPropertyName("logradouro")]
+    public required string Logradouro { get; set; }
+    
+    [JsonPropertyName("bairro")]
+    public required string Bairro { get; set; }
+    
+    [JsonPropertyName("localidade")]
+    public required string Localidade { get; set; }
+    
+    [JsonPropertyName("uf")]
+    public required string Uf { get; set; }
+    
+    [JsonPropertyName("estado")]
+    public required string Estado { get; set; }
+    
+    [JsonPropertyName("complemento")]
+    public string? Complemento { get; set; }
+    
+    [JsonPropertyName("unidade")]
+    public string? Unidade { get; set; }
+    
+    [JsonPropertyName("regiao")]
+    public string? Regiao { get; set; }
+    
+    [JsonPropertyName("ibge")]
+    public string? Ibge { get; set; }
+    
+    [JsonPropertyName("gia")]
+    public string? Gia { get; set; }
+    
+    [JsonPropertyName("ddd")]
+    public string? Ddd { get; set; }
+    
+    [JsonPropertyName("siafi")]
+    public string? Siafi { get; set; }
 }
